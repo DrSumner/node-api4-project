579136c8832b600234f8cafea9efb82414319c3c
DrSumner <157843168+DrSumner@users.noreply.github.com>
2024-07-08 14:53:48 -0400

    users

--- index.js
+++ index.js
@@ -11,10 +11,21 @@ const port = process.env.PORT || 9000
 server.use(express.json())
 server.use(cors())
 
+const userData = [
+    {name: 'Bob', age:'20', id: 0},
+    {name: 'John', age:'21', id: 1},
+    {name: 'Jane', age:'19', id: 2},
+]
+
+
 server.listen(port, () => {
     console.log('listening on', port)
 })
 
 server.get('/api/test', (req, res) => {
     res.json({message:'the test works'})
+})
+
+server.get('/api/users', (req, res) => {
+    res.json(userData)
 })
